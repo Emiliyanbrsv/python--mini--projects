da031f156a0bdb6d23763ebfc1613d512cdc082f
Emiliyan Borisov <emiliyan.borisov28@gmail.com>
2023-09-19 16:45:51 +0300

    updated gender classifier

--- gender_classifier/gender_classifier.py
+++ gender_classifier/gender_classifier.py
@@ -1,5 +1,5 @@
-from sklearn import tree
-from sklearn import svm
+from sklearn import tree, svm, neighbors
+import pandas as pd
 
 # [height, weight, shoe size]
 X = [[181, 80, 44], [177, 70, 43], [160, 60, 38], [154, 54, 37], [166, 65, 40], [190, 90, 47], [175, 64, 39],
@@ -7,26 +7,44 @@ X = [[181, 80, 44], [177, 70, 43], [160, 60, 38], [154, 54, 37], [166, 65, 40], 
 
 Y = ['male', 'male', 'female', 'female', 'male', 'male', 'female', 'female', 'female', 'male', 'male']
 
-
 # Classifiers
 clf_tree = tree.DecisionTreeClassifier()
 clf_svm = svm.SVC()
-
+clf_neighbors = neighbors.KNeighborsClassifier()
 
 # Training the models
 clf_tree.fit(X, Y)
 clf_svm.fit(X, Y)
+clf_neighbors.fit(X, Y)
 
+data = []
 
-# Testing with new data
-height = int(input('Please enter Height(cm): '))
-weight = int(input('Please enter Weight(kg): '))
-shoe_size = int(input('Please enter Shoe size(eu): '))
+while True:
+    height = input('Please enter Height(cm) (or enter q to stop): ')
+    if height.lower() == 'q':
+        break
 
-print()
+    # Convert height to int
+    height = int(height)
+    weight = int(input('Please enter Weight(kg): '))
+    shoe_size = int(input('Please enter Shoe size(EU): '))
+    expected_output = input('Enter expected output: ')
 
-prediction_clf_tree = clf_tree.predict([[height, weight, shoe_size]])
-print(f"Tree Classifier prediction: {prediction_clf_tree[0]}")
+    print()
 
-prediction_clf_svm = clf_svm.predict([[height, weight, shoe_size]])
-print(f"SVM Classifier prediction: {prediction_clf_svm[0]}")
+    # Store the input data
+    data.append([height, weight, shoe_size, expected_output])
+
+# Create a DataFrame with the collected data
+df = pd.DataFrame(data, columns=['Height(cm)', 'Weight(kg)', 'Shoe size(EU)', 'Expected Gender'])
+
+# Add prediction columns to the DataFrame
+
+df['Tree Prediction'] = clf_tree.predict(df[['Height(cm)', 'Weight(kg)', 'Shoe size(EU)']].values)
+df['SVM Prediction'] = clf_svm.predict(df[['Height(cm)', 'Weight(kg)', 'Shoe size(EU)']].values)
+df['KNeighbors Prediction'] = clf_neighbors.predict(df[['Height(cm)', 'Weight(kg)', 'Shoe size(EU)']].values)
+
+# Display the DataFrame
+# print(df)
+df.to_excel('gender_classifier.xlsx', index=False)
+
